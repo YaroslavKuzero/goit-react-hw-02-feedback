a73e8a2fa811f80338ac20eeb1afdd64573e292d
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2020-07-10 18:01:45 +0300

    Updates

---
+++ precache-manifest.12360922ce1b1b8628d9c82cf43110c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5500b244c059fb51f28cc4105a58b6e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a5a921008eaf37a60a9d",
+    "url": "/goit-react-hw-01-components/static/css/main.5970a3f0.chunk.css"
+  },
+  {
+    "revision": "04501688b0ac7ab34b23",
+    "url": "/goit-react-hw-01-components/static/js/2.896411bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.896411bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a921008eaf37a60a9d",
+    "url": "/goit-react-hw-01-components/static/js/main.8cfc4a06.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
